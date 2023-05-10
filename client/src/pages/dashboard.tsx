6c65e5867ffcc6dadff50c995b6fb6c491f540b9
Gabriel Lafrance <105870085+Slimy992@users.noreply.github.com>
2023-05-10 10:47:22 -0400

    Bases

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { ClockIcon, CloudArrowDownIcon, CpuChipIcon } from "@heroicons/react/24/solid"
 
 export const Dashboard = () => {
     return (
@@ -6,14 +7,29 @@ export const Dashboard = () => {
         <div className=" h-1 bg-white w-full mb-8 mt-2"/>
 
         <div className="h-fit">
-            <div className="grid lg:grid-cols-3 gap-5 mb-16">
-            <div className="rounded bg-white h-40 shadow-sm"></div>
-            <div className="rounded bg-white h-40 shadow-sm"></div>
-            <div className="rounded bg-white h-40 shadow-sm"></div>
+            <div className="grid lg:grid-cols-3 gap-5 my-16">
+                <div className="rounded-2xl bg-green-400 h-32 shadow-sm flex justify-center items-center border-2 border-white">
+                     <div className=" w-full h-24 flex justify-center mt-8">
+                        <ClockIcon className="h-16 w-16 text-green-800 mr-4"/>
+                        <span className="text-secondary font-medium text-lg my-[2%]">Uptime : 99.9%</span>
+                     </div>
+                </div>
+                <div className="rounded-2xl bg-red-400 h-32 shadow-sm flex justify-center items-center border-2 border-white">
+                     <div className=" w-full h-24 flex justify-center mt-8">
+                        <CloudArrowDownIcon className="h-16 w-16 text-red-800 mr-4"/>
+                        <span className="text-secondary font-medium text-lg my-[2%]">Stockage : 99.9%</span>
+                     </div>
+                </div>
+                <div className="rounded-2xl bg-yellow-400 h-32 shadow-sm flex justify-center items-center border-2 border-white">
+                     <div className=" w-full h-24 flex justify-center mt-8">
+                        <CpuChipIcon  className="h-16 w-16 text-yellow-800 mr-4"/>
+                        <span className="text-secondary font-medium text-lg my-[2%]">CPU Usage : 49.99%</span>
+                     </div>
+                </div>
             </div>
         </div>
-        <div className="  h-4/6">
-            <div className="grid col-1 bg-white h-full shadow-sm"></div>
+        <div className="h-4/6">
+            <div className="grid col-1 bg-white rounded-2xl h-5/6 shadow-sm"></div>
         </div>
 
     </div>
